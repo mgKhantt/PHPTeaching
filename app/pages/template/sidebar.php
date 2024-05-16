9c0d0bb1bf9897804cd32b67ac4a2566e1a479ee
mgKhantt <khantphonenaing156@gamil.com>
2024-05-16 20:39:28 +0630

    Testing OOP

---
+++ app/pages/template/sidebar.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body id="sidebar">
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">FDC<sup>13</sup></div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- intro -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Intro</span>
+            </a>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/intro/page1.php">Page1</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/intro/page2.php">Page2</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/intro/page3.php">Page3</a>
+                </div>
+            </div>
+        </li>
+        <!-- intro -->
+
+        <!-- oop -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#oop"
+                aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>OOP</span>
+            </a>
+            <div id="oop" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/encapsulation.php">Encapsulation</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/inheritence.php">Inheritence</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/polymorphism.php">Polymorphism</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/abstract.php">Abstract</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/interface.php">Interface</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/trait.php">Trait</a>
+                    <a class="collapse-item" href="http://localhost/php_fdc13/app/pages/oop/static.php">Static</a>
+                </div>
+            </div>
+        </li>
+        <!-- oop -->
+    </ul>
+</body>
+
+</html>
